f03f91ad41cfbe506fccd4dd05d4470b9258a950
Aleksandr Romanov <sasha.sanya.romanov@mail.ru>
2025-05-27 18:10:14 +0300

    account web server init commit

--- services/account/main.go
+++ services/account/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
-	println("Account service started")
+	port := flag.String("port", "8080", "")
+	flag.Parse()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello")
+	})
+
+	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
